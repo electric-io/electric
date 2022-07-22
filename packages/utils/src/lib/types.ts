ec18adb6dfd29020580d81f5cb13a3232e3a9088
Danny McGee <dannymcgee@gmail.com>
2022-07-21 23:18:43 -0400

    Refactoring

--- packages/utils/src/lib/types.ts
+++ packages/utils/src/lib/types.ts
@@ -1,5 +1,8 @@
 export type Obj = { [key: string]: any };
-export type Predicate<T> = (item: T) => boolean;
+export type Predicate<T> = (value: T) => boolean;
+export type Nullish = undefined | null | void;
+export type Option<T> = Nullish | T;
+export type Transparent<T> = {} & { [K in keyof T]: T[K]; };
 
 export interface Fn<Args extends any[] = [], R = void> {
 	(...args: Args): R;
